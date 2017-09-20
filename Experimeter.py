a684a2155776c7ad86d943ae36f388d5432d0f13
Randall Sewell-Edmonds <rsewell@castelli-us.com>
2017-09-20 16:55:08 -0700

    Some minor improvements. Added ResultsWriter

--- Experimeter.py
+++ Experimeter.py
@@ -1,5 +1,6 @@
 import os
 import time
+import csv
 
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
@@ -24,7 +25,7 @@ def parameter_search():
 
         test_value = (i * scalar)
         # float(str(i / 10))
-        X_train, X_test, Y_train, Y_test = train_test_split(X_yeast, Y_yeast, train_size=0.65, random_state=0)
+        X_train, X_test, Y_train, Y_test = train_test_split(X_yeast, Y_yeast, train_size=0.65)
 
         _estimator = SVC(kernel='linear')
         _estimator.C = (i * scalar)
@@ -64,7 +65,7 @@ print("Adult test dataset shape: ", adult_test.shape)
 # Read Yeast
 yeast_trainFile = open(os.environ['USERPROFILE'] + '//OneDrive//Documents//Education//Grad School//Datasets//yeast.csv')
 yeast = pd.read_csv(yeast_trainFile)
-print("Yeast dataset shape: ", yeast.columns.values)
+print("Yeast dataset shape: ", yeast.shape)
 
 # -- Adult Pre-processing --
 # Concatenate Adult sets
@@ -103,63 +104,92 @@ Y_yeast = yeast.loc[:, 'ClassDist']
 
 
 # -- Classifier setup --
+default_adult_DTree = DecisionTreeClassifier(random_state=13)
+default_adult_knn = KNeighborsClassifier()
+default_adult_adaB = RandomForestClassifier(random_state=13)
+default_adult_MLP = MLPClassifier(random_state=13)
+default_adult_SVC = SVC(random_state=13)
+default_yeast_DTree = DecisionTreeClassifier(random_state=13)
+default_yeast_knn = KNeighborsClassifier()
+default_yeast_RFC = RandomForestClassifier(random_state=13)
+default_yeast_MLP = MLPClassifier( random_state=13)
+default_yeast_SVC = SVC(random_state=13)
+
 adult_gini = DecisionTreeClassifier(criterion="gini",
-                                    random_state=100,
                                     max_depth=60,
                                     min_samples_leaf=50,
-                                    min_samples_split=500)
+                                    min_samples_split=500,
+                                    random_state=13)
 adult_entropy = DecisionTreeClassifier(criterion="entropy",
-                                       random_state=100,
                                        max_depth=10,
                                        min_samples_leaf=50,
-                                       min_samples_split=500)
+                                       min_samples_split=500,
+                                       random_state=13)
 adult_knn = KNeighborsClassifier(n_neighbors=30,
                                  n_jobs=4)
 adult_adaB = RandomForestClassifier(max_depth=20,
                                     min_samples_split=50,
                                     min_samples_leaf=4,
-                                    max_leaf_nodes=500)
+                                    max_leaf_nodes=500,
+                                    random_state=13)
 adult_MLP = MLPClassifier(hidden_layer_sizes=(3, 2, 4),
                           solver='sgd',
-                          max_iter=500)
+                          max_iter=5000,
+                          random_state=13)
 adult_SVC = SVC(C=1.0,
                 kernel='rbf',
                 verbose=False,
                 max_iter=-1,
-                cache_size=1000)
+                cache_size=1000,
+                random_state=13)
 yeast_gini = DecisionTreeClassifier(criterion="gini",
                                     max_depth=9,
                                     min_samples_leaf=10,
-                                    min_samples_split=50)
+                                    min_samples_split=50,
+                                    random_state=13)
 yeast_entropy = DecisionTreeClassifier(criterion="entropy",
                                        max_depth=10,
                                        min_samples_split=30,
-                                       min_samples_leaf=30)
+                                       min_samples_leaf=30,
+                                       random_state=13)
 yeast_knn = KNeighborsClassifier(n_neighbors=18,
                                  n_jobs=4)
 yeast_RFC = RandomForestClassifier(max_depth=8,
                                    min_samples_split=50,
                                    min_samples_leaf=10,
-                                   max_leaf_nodes=100)
-yeast_MLP = MLPClassifier(max_iter=4000)
-yeast_SVC = SVC(C=10,
-                kernel='linear',
+                                   max_leaf_nodes=100,
+                                   random_state=13)
+yeast_MLP = MLPClassifier(max_iter=5000,
+                          random_state=13)
+yeast_SVC = SVC(C=1.0,
+                kernel='rbf',
                 verbose=False,
                 max_iter=-1,
-                cache_size=1000)
+                cache_size=1000,
+                random_state=13)
 
-clf_dict = {'Adult_gini DTree': adult_gini,
-            'Adult_entropy_Tree': adult_entropy,
-            'Adult_k-NN': adult_knn,
-            'Adult_RandomForest': adult_adaB,
-            'Adult_Perceptron_ANN': adult_MLP,
-            'Adult_SVC': adult_SVC,
-            'Yeast_gini_DTree': yeast_gini,
-            'Yeast_entropy_Tree': yeast_entropy,
-            'Yeast_k-NN': yeast_knn,
-            'Yeast_RandomForest': yeast_RFC,
-            'Yeast_Perceptron_ANN': yeast_MLP,
-            'Yeast_SVC': yeast_SVC}
+clf_dict = {'Adult default DTree': default_adult_DTree,
+            'Adult default k-NN': default_adult_knn,
+            'Adult default RandomForest': default_adult_adaB,
+            'Adult default Perceptron_ANN': default_adult_MLP,
+            'Adult default SVC': default_adult_SVC,
+            'Yeast default DTree': default_yeast_DTree,
+            'Yeast default k-NN': default_yeast_knn,
+            'Yeast default RandomForest': default_yeast_RFC,
+            'Yeast default Perceptron_ANN': default_yeast_MLP,
+            'Yeast default SVC': default_yeast_SVC,
+            'Adult gini DTree': adult_gini,
+            'Adult entropy_Tree': adult_entropy,
+            'Adult k-NN': adult_knn,
+            'Adult RandomForest': adult_adaB,
+            'Adult Perceptron_ANN': adult_MLP,
+            'Adult SVC': adult_SVC,
+            'Yeast gini DTree': yeast_gini,
+            'Yeast entropy Tree': yeast_entropy,
+            'Yeast k-NN': yeast_knn,
+            'Yeast RandomForest': yeast_RFC,
+            'Yeast Perceptron_ANN': yeast_MLP,
+            'Yeast SVC': yeast_SVC}
 
 # -- Parameter Testser --
 # parameter_search()
@@ -167,8 +197,13 @@ clf_dict = {'Adult_gini DTree': adult_gini,
 
 # -- Classifier fitting --
 # Results Header
-template = "{0:25}{1:15}{2:15}{3:15}"
+template = "{0:35}{1:15}{2:15}{3:15}"
 print(template.format("\nClassifier", "Accuracy (%)", "Learning Time (s)", "Predict Time (s)"))
+
+output_file = open('.//results//' + 'hw1_results.csv', 'w', newline='')
+csv_writer = csv.writer(output_file, delimiter=',')
+csv_writer.writerow(["Classifier", "Accuracy (%)", "Learning Time (s)", "Predict Time (s)"])
+
 for key, value in clf_dict.items():
     if key[0:5] == 'Adult':
         X = X_adult
@@ -178,11 +213,14 @@ for key, value in clf_dict.items():
         Y = Y_yeast
 
     if key[-3:] == 'SVC':
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=200)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=200, random_state=13)
         X_train = scale(X_train)
         X_test = scale(X_test)
     else:
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.45)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.45, random_state=13)
+        if key[-3:] == 'ANN':
+            X_train = scale(X_train)
+            X_test = scale(X_test)
 
     start = time.time()
     value.fit(X_train, Y_train)
@@ -192,10 +230,16 @@ for key, value in clf_dict.items():
     prediction = (accuracy_score(Y_test, value.predict(X_test)))
     end_predict = time.time()
 
+    # Display Results
     print(template.format(key,
                           "%.2f" % (prediction * 100),
                           "%.2f" % (end - start),
                           "%.2f" % (end_predict - start_predict)))
+
+    # Write results file
+    csv_writer.writerow([key, prediction, (end - start), (end_predict - start_predict)])
+
+output_file.close()
 
 # -- Results Plotting --
 for key, value in clf_dict.items():
@@ -208,13 +252,16 @@ for key, value in clf_dict.items():
 
     if key[-3:] == 'SVC':
         X = scale(X)
-        cv = ShuffleSplit(n_splits=20, train_size=200)
+        cv = ShuffleSplit(n_splits=5, train_size=200, random_state=13)
     else:
-        cv = ShuffleSplit(n_splits=100, test_size=0.45)
+        cv = ShuffleSplit(n_splits=5, test_size=0.45, random_state=13)
+        if key[-3:] == 'ANN':
+            X = scale(X)
 
     start_plot = time.time()
     plt = plot_learning_curve(value, key, X, Y, ylim=(0.0, 1.01), cv=cv, n_jobs=1)
     end_plot = time.time()
 
-    plt.savefig(key)
+    plt.savefig('.//results//' + key)
     print(key, "figure complete. Plot time: " + "%.2f" % (end_plot - start_plot))
+    plt.close(key)
